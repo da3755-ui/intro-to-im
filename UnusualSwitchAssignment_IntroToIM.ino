84cc7d7002b1b2e0cfad3839631bc4e4fa16566c
da3755-ui <da3755@nyu.edu>
2026-03-31 00:15:53 +0400

    Add files via upload

---
+++ UnusualSwitchAssignment_IntroToIM.ino
@@ -0,0 +1,19 @@
+const int book = 2;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(book, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int state = digitalRead(book);
+
+  if (state == LOW) {   
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  // digitalWrite(ledPin, HIGH);
+}
+
